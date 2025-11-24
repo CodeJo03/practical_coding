b69a8d9fbe183843dbc6329014fb178fe2f2ae31
Gzoro <woo15907@ajou.ac.kr>
2025-11-25 00:27:18 +0900

    첫 강의자료 코드

---
+++ app.js
@@ -0,0 +1,45 @@
+var express = require('express')
+const logger = require('morgan')
+const axios = require('axios');
+
+var app = express()
+const port = 3000
+
+app.use(express.json())
+app.use(express.urlencoded({extended : true}));
+app.use(logger('dev'));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/user/:id', (req,res) => {
+    res.send(`User id is ${req.params.id}`);
+})
+
+app.get('/user', (req,res) => {
+    res.send(`User id is ${req.query.id}`);
+})
+
+app.post('/user', (req,res) => {
+    console.log(req.body.name);
+    res.send(req.body);
+})
+
+app.get('music_list', (req,res) => {
+    res.json(list);
+})
+
+app.get('/musicSearch/:term', async (req,res) =>{
+    const params = {
+        term : req.params.term,
+        entity : "album",
+    }
+    var response = await axios.get("https://itunes.apple.com/search", {params : params});
+    console.log(response.data);
+    res.json(response.data);
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
